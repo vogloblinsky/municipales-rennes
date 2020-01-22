325e7ad406eecc80161caf7acf71cfda3b80b66d
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2020-01-22 21:36:52 +0100

    fix(app): nav design

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('hello');
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        console.log('hello');
+        const navContainer = document.querySelector('.nav-container');
+        const mainContainer = document.querySelector('.main-container');
+        mainContainer.style.top = navContainer.offsetHeight + 'px';
+    }, 500);
+});
