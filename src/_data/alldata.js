61a30fe25755fa19ce4f2d71183e707100bc9da3
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2020-01-22 15:18:44 +0100

    feat(data): merge thematiques and sous-thematiques

--- src/_data/alldata.js
+++ src/_data/alldata.js
@@ -1,5 +1,19 @@
+let thematiques = require('./thematiques.json');
+let sousthematiques = require('./sous-thematiques.json');
+
+thematiques.forEach(thematique => {
+    if (thematique['sous-thematiques']) {
+        thematique['sous-thematiques'] = thematique['sous-thematiques'].map(
+            ssthematiqueid => {
+                return sousthematiques.find(
+                    sousthematique => sousthematique.id === ssthematiqueid
+                );
+            }
+        );
+    }
+});
+
 module.exports = {
     candidats: require('./candidats.json'),
-    thematiques: require('./thematiques.json'),
-    sousthematiques: require('./sous-thematiques.json')
+    thematiques: thematiques
 };
