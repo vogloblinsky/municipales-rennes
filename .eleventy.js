42b4d780cd792fbb101791fc86d69a338154f042
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2020-01-23 22:45:58 +0100

    feat(app): horizontal scroll + header

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,59 @@
 module.exports = function(eleventyConfig) {
     //copy files
     eleventyConfig.addPassthroughCopy('./src/assets/');
+
+    eleventyConfig.addHandlebarsHelper('compare', function(
+        lvalue,
+        rvalue,
+        options
+    ) {
+        if (arguments.length < 3)
+            throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
+
+        operator = options.hash.operator || '==';
+
+        var operators = {
+            '==': function(l, r) {
+                return l == r;
+            },
+            '===': function(l, r) {
+                return l === r;
+            },
+            '!=': function(l, r) {
+                return l != r;
+            },
+            '<': function(l, r) {
+                return l < r;
+            },
+            '>': function(l, r) {
+                return l > r;
+            },
+            '<=': function(l, r) {
+                return l <= r;
+            },
+            '>=': function(l, r) {
+                return l >= r;
+            },
+            typeof: function(l, r) {
+                return typeof l == r;
+            }
+        };
+
+        if (!operators[operator])
+            throw new Error(
+                "Handlerbars Helper 'compare' doesn't know the operator " +
+                    operator
+            );
+
+        var result = operators[operator](lvalue, rvalue);
+
+        if (result) {
+            return options.fn(this);
+        } else {
+            return options.inverse(this);
+        }
+    });
+
     return {
         dir: { input: 'src', output: 'dist' }
     };
--- src/index.js
+++ src/index.js
@@ -4,5 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
         const navContainer = document.querySelector('.nav-container');
         const mainContainer = document.querySelector('.main-container');
         mainContainer.style.top = navContainer.offsetHeight + 'px';
+
+        const candidatBlocks = Array.from(
+            mainContainer.querySelectorAll('article')
+        );
+        candidatBlocks.map(candidatBlock => {
+            candidatBlock.style.width = mainContainer.offsetWidth + 'px';
+        });
     }, 500);
 });
