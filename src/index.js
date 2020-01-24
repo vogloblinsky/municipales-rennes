09ed016e4e2d4be4c763ca29b287c859b69bc870
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2020-01-24 22:08:02 +0100

    feat(app): horizontal scroll buttons click

--- src/index.js
+++ src/index.js
@@ -1,15 +1,67 @@
-document.addEventListener('DOMContentLoaded', () => {
-    setTimeout(() => {
-        console.log('hello');
-        const navContainer = document.querySelector('.nav-container');
-        const mainContainer = document.querySelector('.main-container');
-        mainContainer.style.top = navContainer.offsetHeight + 'px';
+const initUI = () => {
+    const maxWidthCandidatBlock = 380;
+    let currentWidthForCandidat = 0;
 
-        const candidatBlocks = Array.from(
-            mainContainer.querySelectorAll('article')
+    const navContainer = document.querySelector('.nav-container');
+    const mainContainer = document.querySelector('.main-container');
+
+    const mainContainerWidth = mainContainer.offsetWidth;
+
+    mainContainer.style.top = navContainer.offsetHeight + 'px';
+
+    const candidatBlocks = Array.from(
+        mainContainer.querySelectorAll('article')
+    );
+    candidatBlocks.map(candidatBlock => {
+        if (mainContainerWidth > maxWidthCandidatBlock) {
+            currentWidthForCandidat = maxWidthCandidatBlock;
+        } else {
+            currentWidthForCandidat = mainContainerWidth;
+        }
+        candidatBlock.style.width = currentWidthForCandidat + 'px';
+    });
+
+    // nav buttons
+    let currentCandidatIndex = 0;
+    const nbCandidats = candidatBlocks.length;
+    const leftButton = document.querySelectorAll(
+        '.second-nav__right .button'
+    )[0];
+    const rightButton = document.querySelectorAll(
+        '.second-nav__right .button'
+    )[1];
+
+    const translateCandidats = e => {
+        if (e.currentTarget.classList.contains('disabled')) return;
+        const direction = parseInt(
+            e.currentTarget.getAttribute('data-direction')
         );
-        candidatBlocks.map(candidatBlock => {
-            candidatBlock.style.width = mainContainer.offsetWidth + 'px';
-        });
-    }, 500);
+        if (direction === 1) {
+            currentCandidatIndex += 1;
+            mainContainer.scrollLeft += currentWidthForCandidat;
+        } else {
+            currentCandidatIndex -= 1;
+            mainContainer.scrollLeft -= currentWidthForCandidat;
+        }
+        if (
+            currentCandidatIndex > 0 &&
+            currentCandidatIndex < nbCandidats - 1
+        ) {
+            leftButton.classList.remove('disabled');
+            rightButton.classList.remove('disabled');
+        } else if (currentCandidatIndex === 0) {
+            leftButton.classList.add('disabled');
+            rightButton.classList.remove('disabled');
+        } else {
+            leftButton.classList.remove('disabled');
+            rightButton.classList.add('disabled');
+        }
+    };
+
+    leftButton.addEventListener('click', translateCandidats);
+    rightButton.addEventListener('click', translateCandidats);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(initUI, 500);
 });
