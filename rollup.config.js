9ff887e015a7ace6ca5d2bf83a176a729d7dc20d
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2020-01-26 15:37:48 +0100

    feat(app): resolve commonjs data file

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import resolve from 'rollup-plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import json from '@rollup/plugin-json';
 
 const ENV = process.env.ENV || 'dev';
 
-const PLUGINS = [resolve(), json()];
+const PLUGINS = [resolve(), commonjs(), json()];
 
 if (ENV !== 'dev') {
     PLUGINS.push(terser());
