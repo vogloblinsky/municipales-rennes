02e27dd0e61920b4ff3a689f4209017d55f1455b
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2020-01-26 01:47:03 +0100

    feat(app): images optimization

---
+++ tools/images-optimization.js
@@ -0,0 +1,29 @@
+var compress_images = require('compress-images'),
+    INPUT_path_to_your_images,
+    OUTPUT_path;
+
+INPUT_path_to_your_images = 'dist/assets/**/*.{jpg,JPG,jpeg,JPEG,png}';
+OUTPUT_path = 'dist2/assets/';
+
+compress_images(
+    INPUT_path_to_your_images,
+    OUTPUT_path,
+    { compress_force: false, statistic: true, autoupdate: true },
+    false,
+    { jpg: { engine: 'mozjpeg', command: ['-quality', '60'] } },
+    { png: { engine: 'pngquant', command: ['--quality=20-50'] } },
+    { svg: { engine: 'svgo', command: '--multipass' } },
+    {
+        gif: {
+            engine: 'gifsicle',
+            command: ['--colors', '64', '--use-col=web']
+        }
+    },
+    function(error, completed, statistic) {
+        console.log('-------------');
+        /*console.log(error);
+        console.log(completed);
+        console.log(statistic);*/
+        console.log('-------------');
+    }
+);
