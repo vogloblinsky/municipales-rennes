1cf2b6621a826af6fc0de7de424490e7e459932b
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2020-01-28 04:18:13 +0100

    fix(tools): es5 build

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import json from '@rollup/plugin-json';
 
@@ -7,15 +8,26 @@ const ENV = process.env.ENV || 'dev';
 
 const PLUGINS = [resolve(), commonjs(), json()];
 
-if (ENV !== 'dev') {
-    PLUGINS.push(terser());
-}
-
-export default {
-    input: 'src/index.js',
-    output: {
-        file: 'dist/scripts/index.js',
-        format: 'iife'
-    },
-    plugins: PLUGINS
+const createConfig = type => {
+    let MAIN_CONFIG = {
+        input: 'src/index.js',
+        output: {
+            file: 'dist/scripts/index.js',
+            format: 'es'
+        },
+        plugins: [resolve(), commonjs(), json()]
+    };
+    if (type === 'es5') {
+        MAIN_CONFIG.output.file = 'dist/scripts/index_es5.js';
+        MAIN_CONFIG.output.format = 'iife';
+        MAIN_CONFIG.plugins.push(babel());
+    }
+    if (ENV !== 'dev') {
+        MAIN_CONFIG.plugins.push(terser());
+    }
+    return MAIN_CONFIG;
 };
+
+const CONFIG = [createConfig('es6'), createConfig('es5')];
+
+export default CONFIG;
