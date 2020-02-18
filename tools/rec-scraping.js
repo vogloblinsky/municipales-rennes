009c5a70bac13daa04dd06cbc014bb1da5c04789
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2020-02-18 06:26:13 +0100

    feat(tools): rec scraping script

---
+++ tools/rec-scraping.js
@@ -0,0 +1,46 @@
+const puppeteer = require('puppeteer');
+const path = require('path');
+const fs = require('fs-extra');
+
+const URL = 'https://rennes-en-commun-2020.fr/programme-des-municipales-2020-rennes-en-commun-partie1-la-laicite';
+const candidatIndex = 4;
+const sousThematiqueId = 'st-17';
+const propositionPrefixId = 'lepape-p-';
+
+const JSON = '../src/_data/candidats.json';
+
+const data = require(JSON);
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(URL);
+
+    let startIndex = parseInt(data[candidatIndex].propositions.slice(-1)[0].id.replace(propositionPrefixId, ''));
+
+    const propositions = await page.evaluate(
+        (propositionPrefixId, sousThematiqueId, startIndex) => {
+            let propositions = Array.from(document.querySelectorAll('.eut-message'));
+            return propositions.map((proposition, index) => {
+                return {
+                    id: propositionPrefixId + (startIndex + index + 1),
+                    st: sousThematiqueId,
+                    label: proposition.innerText
+                };
+            });
+        },
+        propositionPrefixId,
+        sousThematiqueId,
+        startIndex
+    );
+
+    data[candidatIndex].propositions = [...data[candidatIndex].propositions, ...propositions];
+
+    console.log(propositions);
+
+    fs.writeJson(path.join(__dirname, JSON), data, async err => {
+        if (err) return console.error(err);
+        console.log('JSON write success!');
+        await browser.close();
+    });
+})();
