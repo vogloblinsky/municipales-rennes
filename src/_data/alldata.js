16f563933a39e2ae712fa8d39e928af4885b1f37
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2020-02-24 17:19:34 +0000

    feat(app): data update

--- src/_data/alldata.js
+++ src/_data/alldata.js
@@ -8,9 +8,7 @@ const mergeThematiquesAndSousThematiques = () => {
         if (thematique['sous-thematiques']) {
             thematique['sous-thematiques'] = thematique['sous-thematiques']
                 .map(ssthematiqueid => {
-                    return sousthematiques.find(
-                        sousthematique => sousthematique.id === ssthematiqueid
-                    );
+                    return sousthematiques.find(sousthematique => sousthematique.id === ssthematiqueid);
                 })
                 .filter(ssthematique => ssthematique !== undefined);
         }
@@ -27,9 +25,7 @@ candidats.forEach(candidat => {
 // 2. filtrage des sous-thematiques non utilisées
 
 sousthematiques = sousthematiques.filter(sousthematique => {
-    return mergedPropositions.find(
-        proposition => proposition.st === sousthematique.id
-    );
+    return mergedPropositions.find(proposition => proposition.st === sousthematique.id);
 });
 
 // 3. merge des thematiques et sous-thematiques
@@ -40,21 +36,19 @@ candidats.forEach(candidat => {
     candidat.orderedThematiques = JSON.parse(JSON.stringify(thematiques));
     candidat.orderedThematiques.forEach(candidatThematique => {
         if (candidatThematique['sous-thematiques']) {
-            candidatThematique['sous-thematiques'].forEach(
-                candidatSousThematique => {
-                    if (candidatSousThematique) {
-                        candidatSousThematique.propositions = [];
-                        candidatSousThematique.propositions = candidat.propositions.filter(
-                            candidatProposition => {
-                                return (
-                                    candidatProposition.st ===
-                                    candidatSousThematique.id
-                                );
-                            }
-                        );
-                    }
+            candidatThematique['sous-thematiques'].forEach(candidatSousThematique => {
+                if (candidatSousThematique) {
+                    candidatSousThematique.propositions = [];
+                    candidatSousThematique.propositions = candidat.propositions.filter(candidatProposition => {
+                        return candidatProposition.st === candidatSousThematique.id;
+                    });
+                    candidatSousThematique.propositions = candidatSousThematique.propositions.sort(
+                        (proposition1, proposition2) => {
+                            return !proposition1.important;
+                        }
+                    );
                 }
-            );
+            });
         }
     });
 });
